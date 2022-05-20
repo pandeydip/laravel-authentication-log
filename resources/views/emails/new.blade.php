80c41bf688b883232eb3096d6c63c63498257c21
Deepak Pandey <pandey.dip@gmail.com>
2022-05-20 17:35:33 +0545

    add location country

--- resources/views/emails/new.blade.php
+++ resources/views/emails/new.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
 # @lang('Hello!')
 
-@lang('Your :app account logged in from a new device.', ['app' => config('app.name')])
+@lang('Your :app account logged in from a new device.', ['app' => $account->name])
 
 > **@lang('Account:')** {{ $account->email }}<br/>
 > **@lang('Time:')** {{ $time->toCookieString() }}<br/>
 > **@lang('IP Address:')** {{ $ipAddress }}<br/>
 > **@lang('Browser:')** {{ $browser }}<br/>
-@if ($location && $location['default'] === false)
-> **@lang('Location:')** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['state'], __('Unknown State') }}
+@if (!is_null($location))
+> **@lang('Location:')** {{ $location['city'] ?? __('Unknown City') }}, {{ $location['regionName'], __('Unknown State') }}, {{ $location['country'], __('Unknown Country') }}
 @endif
 
 @lang('If this was you, you can ignore this alert. If you suspect any suspicious activity on your account, please change your password.')
